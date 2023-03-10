2bb77d8a7301de8cb4acbfc57105404194971a0c
Matt Daly <matt.tyler.daly@gmail.com>
2023-03-10 18:05:11 -0500

    Update 18:04 3/10/2023

--- ProjectFolder/FinalProject/App.js
+++ ProjectFolder/FinalProject/App.js
@@ -1,11 +1,37 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  Button,
+} from "react-native";
 
 export default function App() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("./assets/Background.png")}
+        resizeMode="cover"
+        style={styles.background}
+      >
+        <Image
+          source={require("./assets/The_Emperor_Protects.png")}
+          style={{ width: 210, height: 84 }}
+        />
+      </ImageBackground>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
